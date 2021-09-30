f1c7aa8d3d4a8a9a49b1c686f6feec4d29f5aedf
Toxic Dee Modder <90701056+ToxicDeeModder1@users.noreply.github.com>
2021-09-30 23:53:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ app = Client(
 
 @app.on_message(filters.command("start") & filters.private & ~filters.edited)
 async def start_command(_, m: Message):
-    await m.reply_text("Hi, I am Heroku app.json maker bot.\n\n"
+    await m.reply_text("✋Hi, I am Heroku app.json maker bot.\n\n"
                        "To start making app.json for your heroku app,\n"
-                       "Press /f", quote=True, disable_web_page_preview=True)
+                       "Press /f\n\n🔵Developed By @ToxicDeeModderr", quote=True, disable_web_page_preview=True)
 
 
 @app.on_message(filters.command("f") & ~filters.edited & filters.private)
@@ -35,7 +35,7 @@ async def f_command(bot: Client, m: Message):
             await bot.send_document(
                 chat_id=m.chat.id,
                 document=app_json,
-                caption="**Made by @HerokuAppJson_Bot**"
+                caption="**Made by @TheBotsWorldChannel**"
             )
             await editable.edit("Sent `app.json` !!")
             os.remove(app_json)
